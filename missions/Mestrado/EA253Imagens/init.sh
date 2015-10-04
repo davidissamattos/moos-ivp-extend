516a9c4ef1aa5f60f8b16bf08a616d19070c2100
David Issa Mattos <issamattos.david@gmail.com>
2015-10-04 17:44:55 -0300

    adding some folders

---
+++ missions/Mestrado/EA253Imagens/init.sh
@@ -0,0 +1,19 @@
+export PATH=$PATH:/Users/David/moos-ivp/bin
+export PATH=$PATH:/Users/David/moos-ivp-extend/bin
+which MOOSDB
+which uTimerScript
+which mykill
+
+
+killall MOOSDB
+killall pLogger
+killall pHelmIvP
+killall pMarinePID
+killall uProcessWatch
+killall uSimMarine
+killall pNodeReporter
+killall pControleBarco
+killall uBarcoDouglas
+killall iSerial
+
+echo FIM
