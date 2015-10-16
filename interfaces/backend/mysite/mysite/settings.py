395016cceffac071645d1f563ccf4cdec4529b4f
David Issa Mattos <issamattos.david@gmail.com>
2015-10-16 09:24:23 -0300

    Adding Django with Bootstrap

--- interfaces/backend/mysite/mysite/settings.py
+++ interfaces/backend/mysite/mysite/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(__file__)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mod_wsgi.server',
+    'api',
+    'webinterface',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +58,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+    ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +71,11 @@ TEMPLATES = [
         },
     },
 ]
+
+
+ADMINS = (
+    ('David Issa Mattos', 'issamattos.david@gmail.com'),
+)
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
@@ -99,4 +108,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mysite/static/webinterface'),]
+
--- interfaces/backend/mysite/mysite/urls.py
+++ interfaces/backend/mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('api.urls')),
+    url(r'^web/', include('webinterface.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
